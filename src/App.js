39d0c65b8d858ddbaaecb5151a55662a63f4e72e
Vladislav Filippov <vladislav.filippov.01@mail.ru>
2022-04-25 00:04:35 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'styled-components';
+import Container from './components/container';
+import Column from './components/column';
+
+const DeskWrapper = styled.div`
+  border-radius: 16px;
+  background-color: #fff;
+  box-shadow: 2px 3px 12px #6b6d77;
+  height: 600px;
+  width: 100%;
+  max-width: 1300px;
+`;
+
+export default function App() {
+  return (
+    <Container height="100vh">
+      <DeskWrapper>
+        <Container height="100%" justify="flex-start" align="flex-start" margin="40px">
+          <Column tittle="TODO"/>
+          <Column tittle="in progress"/>
+          <Column tittle="testing"/>
+          <Column tittle="Done!"/>
+        </Container >
+      </DeskWrapper>
+    </Container>
+  );
+}
---
+++ src/components/container.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const FelxContainer = styled.div`
+    display: flex;
+    flex-direction: ${props => props.column ? 'column' : 'row'};
+    justify-content: ${props => props.justify || 'center'};
+    align-items: ${props => props.align || 'center'};
+    height: ${props => props.height || 'none'};
+    width: ${props => props.width || '100%'};
+    margin ${props => props.margin || '0'};
+    padding ${props => props.padding || '0'};
+`
+
+const Container = (props) => {
+    return (
+        <FelxContainer {...props} />
+    );
+};
+
+export default Container;
