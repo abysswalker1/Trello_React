2ac260eec8a0d93771ee6d5c537b9ca45e71c7fd
Vladislav Filippov <vladislav.filippov.01@mail.ru>
2022-04-26 00:27:45 +0500

    delite button

--- src/components/column/addCard.js
+++ src/components/column/addCard.js
@@ -2,50 +2,52 @@ import React, { useEffect } from 'react';
 import Container from '../container';
 import styled from 'styled-components';
 import Button from '../button';
-import "bootstrap-icons/font/bootstrap-icons.css";
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const AddCardButton = styled.button`
-    width: 100%;
-    padding: 6px;
-    display: ${props => props.enable ? 'none' : 'flex'};
-    &:hover {
-        background-color: #457b9d;
-    }
-`
+  width: 100%;
+  padding: 6px;
+  display: ${(props) => (props.enable ? 'none' : 'flex')};
+  &:hover {
+    background-color: #457b9d;
+  }
+`;
 
 const CardName = styled.textarea`
-    width: 100%;
-`
+  width: 100%;
+`;
 
 const AddCard = (props) => {
-    const [active, setActive] = React.useState(false)
-    const [name, setName] = React.useState('')
+  const [active, setActive] = React.useState(false);
+  const [name, setName] = React.useState('');
 
-    const openForm = (value) => {
-        setActive(value)
-    }
+  const openForm = (value) => {
+    setActive(value);
+  };
 
-    useEffect(() => {
-        openForm(false)
-    },[props.cards])
+  useEffect(() => {
+    openForm(false);
+  }, [props.cards]);
 
-    return (
-        <div>
-            <AddCardButton enable = {active} onClick={() => openForm(true)}>
-                <i className="bi bi-plus-lg"></i>
-                <p style ={{ marginLeft : '7px' }}>Добавить карту</p>
-            </AddCardButton>
-            <Container display={!active ? 'none' : 'block'}>
-                <CardName onChange = {(e) => setName(e.target.value)}/>
-                <Container justify="flex-start">
-                    <Button func = {props.func} value={name}>Добавить</Button>
-                    <button>
-                        <i className="bi bi-x-lg"></i>
-                    </button>
-                </Container>
-            </Container>
-        </div>
-    );
+  return (
+    <div>
+      <AddCardButton enable={active} onClick={() => openForm(true)}>
+        <i className="bi bi-plus-lg"></i>
+        <p style={{ marginLeft: '7px' }}>Добавить карту</p>
+      </AddCardButton>
+      <Container display={!active ? 'none' : 'block'}>
+        <CardName onChange={(e) => setName(e.target.value)} />
+        <Container justify="flex-start">
+          <Button func={props.func} value={name}>
+            Добавить
+          </Button>
+          <button onClick={() => openForm(false)}>
+            <i className="bi bi-x-lg"></i>
+          </button>
+        </Container>
+      </Container>
+    </div>
+  );
 };
 
 export default AddCard;
